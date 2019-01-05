0a629b90769bbe46b69ea825cbc7af0fd14ab57f
Felipe <felipeaugustobento@hotmail.com>
2019-01-05 18:56:36 -0200

    Listando eleições disponível na tela principal

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
 
+import { IEleicao } from 'app/shared/model/eleicao.model';
 import { LoginModalService, AccountService, Account } from 'app/core';
+
+import { ITEMS_PER_PAGE } from 'app/shared';
+import { EleicaoService } from '../entities/eleicao/eleicao.service';
 
 @Component({
     selector: 'jhi-home',
@@ -10,16 +15,81 @@ import { LoginModalService, AccountService, Account } from 'app/core';
     styleUrls: ['home.scss']
 })
 export class HomeComponent implements OnInit {
+    eleicaos: IEleicao[];
     account: Account;
     modalRef: NgbModalRef;
+    itemsPerPage: number;
+    links: any;
+    page: any;
+    predicate: any;
+    queryCount: any;
+    reverse: any;
+    totalItems: number;
 
     constructor(
+        protected eleicaoService: EleicaoService,
         private accountService: AccountService,
         private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
-    ) {}
+        private eventManager: JhiEventManager,
+        protected parseLinks: JhiParseLinks,
+        protected jhiAlertService: JhiAlertService
+    ) {
+        this.eleicaos = [];
+        this.itemsPerPage = ITEMS_PER_PAGE;
+        this.page = 0;
+        this.links = {
+            last: 0
+        };
+        this.predicate = 'id';
+        this.reverse = true;
+    }
+
+    loadAll() {
+        this.eleicaoService
+            .query({
+                page: this.page,
+                size: this.itemsPerPage,
+                sort: this.sort()
+            })
+            .subscribe(
+                (res: HttpResponse<IEleicao[]>) => this.paginateEleicaos(res.body, res.headers),
+                (res: HttpErrorResponse) => this.onError(res.message)
+            );
+    }
+
+    reset() {
+        this.page = 0;
+        this.eleicaos = [];
+        this.loadAll();
+    }
+
+    protected paginateEleicaos(data: IEleicao[], headers: HttpHeaders) {
+        this.links = this.parseLinks.parse(headers.get('link'));
+        this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+        for (let i = 0; i < data.length; i++) {
+            this.eleicaos.push(data[i]);
+        }
+        console.log(this.eleicaos);
+    }
+
+    sort() {
+        const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+        if (this.predicate !== 'id') {
+            result.push('id');
+        }
+        return result;
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+
+    trackId(index: number, item: IEleicao) {
+        return item.id;
+    }
 
     ngOnInit() {
+        this.loadAll();
         this.accountService.identity().then(account => {
             this.account = account;
         });
